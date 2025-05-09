c42a7f3d714146ec80824db22f29e21288cbef4a
Sind01 <102220572+Sindoni750760@users.noreply.github.com>
2025-05-09 10:50:24 +0200

    Create Recensioni.java

---
+++ Recensioni.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+public class Recensioni implements Serializable {
+    String id;
+    String idRistoranti;
+    String usernameAutori;
+    String contenuto;
+    int rating;
+    String reply;
+}

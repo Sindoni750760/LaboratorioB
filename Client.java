cfc28c742adf6889a47f0dbf29dc045ff3ede4bd
Sind01 <102220572+Sindoni750760@users.noreply.github.com>
2025-05-09 10:55:00 +0200

    Update Client.java
    Modificato il Client, creando un'iniziale configurazione.

--- Client.java
+++ Client.java
@@ -1,5 +1,28 @@
-public class Client{
-    public static void main(String[] args){
-        
+package client;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static ObjectInputStream ois;
+    private static ObjectOutputStream oos;
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        final String SERVER_ADDRESS = "localhost";
+        final int SERVER_PORT = 12345;
+
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT)) {
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.flush();
+
+            ois = new ObjectInputStream(socket.getInputStream());
+            scanner = new Scanner(System.in);
+
+            System.out.println("Connected to the server.")
+            //TODO: Creare logica del server
+        }
     }
 }
